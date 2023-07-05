d62d858d495d1e3aae6632e17b61007457f61ccf
Gonzalo Potente <gonzalopotente2772@gmail.com>
2023-07-05 17:52:59 -0300

    components creation commit

--- portfolio-2/src/app/app.module.ts
+++ portfolio-2/src/app/app.module.ts
@@ -3,10 +3,24 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { BanerComponent } from './baner/baner.component';
+import { EducationComponent } from './education/education.component';
+import { PersonalInformationComponent } from './personal-information/personal-information.component';
+import { SkillsComponent } from './skills/skills.component';
+import { WorkExperienceComponent } from './work-experience/work-experience.component';
+import { ProjectsComponent } from './projects/projects.component';
+import { ContactComponent } from './contact/contact.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    BanerComponent,
+    EducationComponent,
+    PersonalInformationComponent,
+    SkillsComponent,
+    WorkExperienceComponent,
+    ProjectsComponent,
+    ContactComponent
   ],
   imports: [
     BrowserModule,
